3ac5c75edf3a4283d4dbc82754756e4b8996128e
AtomWave <AtomWave@yandex.ru>
2024-11-30 21:32:31 +0300

    исправляет адреса карточек отелей

--- src/js/hotels.js
+++ src/js/hotels.js
@@ -1,76 +1,87 @@
+const checkPath = window.location.href;
+const hasCatalog = checkPath.indexOf("pages") !== -1;
+
+let path = '';
+
+if (hasCatalog) {
+  path = './../../'
+} else {
+  path = './'
+}
+
 export const hotels = [
   {
-    img: './public/assets/images/avatar-1.webp',
+    img: `${path}public/assets/images/london.webp`,
     subtitle: 'Hotel 1',
     title: 'Danubius Hotel Regents Park',
     price: 150,
     rating: 4.5,
   },
   {
-    img: 'https://i.tez-tour.travel/img/hotels/112768/0.jpg',
+    img: `${path}public/assets/images/london.webp`,
     subtitle: 'Hotel 2',
     title: 'The Resident Soho',
     price: 120,
     rating: 4.7,
   },
   {
-    img: 'https://i.tez-tour.travel/img/hotels/112768/0.jpg',
+    img: `${path}public/assets/images/london.webp`,
     subtitle: 'Hotel 3',
     title: 'London Bridge Hotel',
     price: 100,
     rating: 3.8,
   },
   {
-    img: 'https://i.tez-tour.travel/img/hotels/112768/0.jpg',
+    img: `${path}public/assets/images/london.webp`,
     subtitle: 'Hotel 2',
     title: 'The Resident Soho',
     price: 120,
     rating: 4.7,
   },
   {
-    img: 'https://i.tez-tour.travel/img/hotels/112768/0.jpg',
+    img: `${path}public/assets/images/london.webp`,
     subtitle: 'Hotel 3',
     title: 'London Bridge Hotel',
     price: 100,
     rating: 3.8,
   },
   {
-    img: 'https://i.tez-tour.travel/img/hotels/112768/0.jpg',
+    img: `${path}public/assets/images/london.webp`,
     subtitle: 'Hotel 2',
     title: 'The Resident Soho',
     price: 120,
     rating: 4.7,
   },
   {
-    img: 'https://i.tez-tour.travel/img/hotels/112768/0.jpg',
+    img: `${path}public/assets/images/london.webp`,
     subtitle: 'Hotel 3',
     title: 'London Bridge Hotel',
     price: 100,
     rating: 3.8,
   },
   {
-    img: 'https://i.tez-tour.travel/img/hotels/112768/0.jpg',
+    img: `${path}public/assets/images/london.webp`,
     subtitle: 'Hotel 2',
     title: 'The Resident Soho',
     price: 120,
     rating: 4.7,
   },
   {
-    img: 'https://i.tez-tour.travel/img/hotels/112768/0.jpg',
+    img: `${path}public/assets/images/london.webp`,
     subtitle: 'Hotel 3',
     title: 'London Bridge Hotel',
     price: 100,
     rating: 3.8,
   },
   {
-    img: 'https://i.tez-tour.travel/img/hotels/112768/0.jpg',
+    img: `${path}public/assets/images/london.webp`,
     subtitle: 'Hotel 2',
     title: 'The Resident Soho',
     price: 120,
     rating: 4.7,
   },
   {
-    img: 'https://i.tez-tour.travel/img/hotels/112768/0.jpg',
+    img: `${path}public/assets/images/london.webp`,
     subtitle: 'Hotel 3',
     title: 'London Bridge Hotel',
     price: 100,
