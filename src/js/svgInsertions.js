f7a8d4555d6d42f7ac1a9b4a5e90c9a11a4c584d
AtomWave <AtomWave@yandex.ru>
2024-12-29 21:36:31 +0300

    адаптив хиро

--- src/js/svgInsertions.js
+++ src/js/svgInsertions.js
@@ -4,10 +4,10 @@ import { insertInlineSVG } from './svgUtils.js'; // Импортируем фу�
 
 // Функция для вставки необходимых SVG
 export function insertSVGIcons() {
-  insertInlineSVG('.video-btn__icon', `/assets/vectors/sprite.svg#arrow-right`, 16, 20);
-  insertInlineSVG('label[for="location"]', `/assets/vectors/sprite.svg#location`, 16, 20);
-  insertInlineSVG('label[for="check-in"]', `/assets/vectors/sprite.svg#calendar`, 18, 20);
-  insertInlineSVG('label[for="check-out"]', `/assets/vectors/sprite.svg#calendar`, 18, 20);
+  insertInlineSVG('.video-btn__icon', `/assets/vectors/sprite.svg#arrow-right`, 40, 40);
+  insertInlineSVG('label[for="location"]', `/assets/vectors/sprite.svg#location`, 24, 24);
+  insertInlineSVG('label[for="check-in"]', `/assets/vectors/sprite.svg#calendar`, 24, 24);
+  insertInlineSVG('label[for="check-out"]', `/assets/vectors/sprite.svg#calendar`, 24, 24);
   insertInlineSVG('label[for="rooms-for"]', `/assets/vectors/sprite.svg#person`, 24, 24);
-  insertInlineSVG('.search-button', `/assets/vectors/sprite.svg#search`, 16, 20);
+  insertInlineSVG('.search-button', `/assets/vectors/sprite.svg#search`, 18, 18);
 }
